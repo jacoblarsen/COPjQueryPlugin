c1c4ebc54614462d43a14ac2959ac5a6beea8261
unknown <jac@Kb007190.kb.dk>
2011-11-28 22:03:00 +0100

    initital commit

---
+++ web/js/cop-plugin.js
@@ -0,0 +1,30 @@
+var availableTags2 = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL",
+        "ColdFusion",
+        "Erlang",
+        "Fortran",
+        "Groovy",
+        "Haskell",
+        "Java",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Scala",
+        "Scheme"
+    ];
+
+$(function (){
+    $("#input").autocomplete({
+                source: availableTags
+            });
+});
